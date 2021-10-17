9b071915253b90a257b65f4fd50238766cfdbc42
LuxABrax <lukabr394x@gmail.com>
2021-10-18 00:06:13 +0200

    searches slice

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -6,6 +6,7 @@ import modalReducer from "./modalSlice";
 import usersReducer from "./usersSlice";
 import postsReducer from "./postsSlice";
 import popupReducer from "./popupSlice";
+import searchesReducer from "./searchesSlice";
 
 const rootReducer = combineReducers({
 	auth: authReducer,
@@ -14,6 +15,7 @@ const rootReducer = combineReducers({
 	users: usersReducer,
 	posts: postsReducer,
 	popup: popupReducer,
+	searches: searchesReducer,
 });
 
 export default rootReducer;
---
+++ src/redux/searchesSlice.js
@@ -0,0 +1,141 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const getSearches = createAsyncThunk(
+	"searches/getSearches",
+	async (userId, { rejectWithValue, dispatch }) => {
+		const response = await axios.get(`/searches/${userId}`);
+
+		const data = response.data;
+		// console.log("response: ", await response.data);
+		if (data.success === false) {
+			dispatch(searchesSlice.actions.setErrMessage(data.message));
+			return rejectWithValue(data.message);
+		} else {
+			dispatch(searchesSlice.actions.setErrMessage(""));
+		}
+		return data.data;
+	}
+);
+
+export const addSearch = createAsyncThunk(
+	"searches/addSearch",
+	async ({ userId, sId }, { rejectWithValue, dispatch }) => {
+		const response = await axios.put(`/searches/${userId}/${sId}`);
+
+		const data = response.data;
+		// console.log("response: ", await response.data);
+		if (data.success === false) {
+			dispatch(searchesSlice.actions.setErrMessage(data.message));
+			return rejectWithValue(data.message);
+		} else {
+			dispatch(searchesSlice.actions.setErrMessage(""));
+		}
+		return data.data;
+	}
+);
+
+export const removeSearch = createAsyncThunk(
+	"searches/removeSearch",
+	async ({ userId, sId }, { rejectWithValue, dispatch }) => {
+		const response = await axios.delete(`/searches/${userId}/${sId}`);
+
+		const data = response.data;
+		// console.log("response: ", await response.data);
+		if (data.success === false) {
+			dispatch(searchesSlice.actions.setErrMessage(data.message));
+			// return rejectWithValue(data.message);
+		} else {
+			dispatch(searchesSlice.actions.setErrMessage(""));
+		}
+		return data.data;
+	}
+);
+
+export const removeSearches = createAsyncThunk(
+	"searches/removeSearches",
+	async (userId, { rejectWithValue, dispatch }) => {
+		const response = await axios.delete(`/searches/${userId}`);
+
+		const data = response.data;
+		// console.log("response: ", await response.data);
+		if (data.success === false) {
+			dispatch(searchesSlice.actions.setErrMessage(data.message));
+			// return rejectWithValue(data.message);
+		} else {
+			dispatch(searchesSlice.actions.setErrMessage(""));
+		}
+		return data.data;
+	}
+);
+
+export const searchesSlice = createSlice({
+	name: "searches",
+	initialState: {
+		status: "",
+		searches: [],
+		error: {
+			message: "",
+		},
+		errMessage: "",
+	},
+	reducers: {
+		setErrMessage: (state, action) => {
+			state.errMessage = action.payload;
+		},
+	},
+	extraReducers: {
+		[getSearches.pending]: state => {
+			state.status = "getting searches";
+		},
+		[getSearches.fulfilled]: (state, { payload }) => {
+			state.searches = payload;
+			state.status = "get searches success";
+		},
+		[getSearches.rejected]: (state, { error }) => {
+			state.error.message = error.message;
+			state.status = "get searches failed";
+		},
+
+		[addSearch.pending]: state => {
+			state.status = "adding search";
+		},
+		[addSearch.fulfilled]: (state, { payload }) => {
+			state.searches = payload;
+			state.status = "add search success";
+		},
+		[addSearch.rejected]: (state, { error }) => {
+			state.error.message = error.message;
+			state.status = "add search failed";
+		},
+
+		[removeSearch.pending]: state => {
+			state.status = "removing search";
+		},
+		[removeSearch.fulfilled]: (state, { payload }) => {
+			state.searches = payload;
+			state.status = "remove search success";
+		},
+		[removeSearch.rejected]: (state, { error }) => {
+			state.error.message = error.message;
+			state.status = "remove search failed";
+		},
+
+		[removeSearches.pending]: state => {
+			state.status = "removing searches";
+		},
+		[removeSearches.fulfilled]: (state, { payload }) => {
+			state.searches = payload;
+			state.status = "remove searches success";
+		},
+		[removeSearches.rejected]: (state, { error }) => {
+			state.error.message = error.message;
+			state.status = "remove searches failed";
+		},
+	},
+});
+
+export const { setErrMessage } = searchesSlice.actions;
+
+export const { selSearches } = state => state.searches.searches;
+
+export default searchesSlice.reducer;
